be7e908c17b410ce5f466cfcfa3e603c6fd19149
Eddie Rusinskas <erusinskas@wonde.com>
2023-01-17 12:40:23 +0000

    init

--- dist/index.js
+++ dist/index.js
@@ -9782,11 +9782,11 @@ try {
         pull_number: github.context.payload.pull_request.number,
     }
 
-    const body = github.context.payload.pull_request.body || '';
-
-    // request.body
-
-    core.setOutput(body.indexOf("#JIRA Ticket\n..."));
+    // const body = github.context.payload.pull_request.body || '';
+    const ticket = core.getInput("host") + "browse/" + core.getInput("board") + "-999";
+    request.body = ({
+        prefix: `# Jira ticket\n${ticket}`
+    })
 
 } catch (error) {
     core.setFailed(error.message);
