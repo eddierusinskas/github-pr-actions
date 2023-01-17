b72e6dc2df29e5a33b379188f48a9725389675b4
Eddie Rusinskas <erusinskas@wonde.com>
2023-01-17 15:20:47 +0000

    Test

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-module.exports = async function run() {
-    try {
-        const request = {
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            pull_number: github.context.payload.pull_request.number,
-        }
-
-        // const body = github.context.payload.pull_request.body || '';
-        const ticket = core.getInput("host") + "browse/" + core.getInput("board") + "-999";
-        request.body = ({
-            prefix: `# Jira ticket\n${ticket}`
-        })
-
-    } catch (error) {
-        core.setFailed(error.message);
+try {
+    const request = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: github.context.payload.pull_request.number,
     }
+
+    // const body = github.context.payload.pull_request.body || '';
+    const ticket = core.getInput("host") + "browse/" + core.getInput("board") + "-999";
+    request.body = ({
+        prefix: `# Jira ticket\n${ticket}`
+    })
+
+} catch (error) {
+    core.setFailed(error.message);
 }
