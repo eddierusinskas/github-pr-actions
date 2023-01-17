747d63ca94c92bfa48e666165c0795dd1f3fbde0
Eddie Rusinskas <erusinskas@wonde.com>
2023-01-17 15:28:57 +0000

    Test

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    const inputs = {
+        host: core.getInput('jira-host', {required: true}),
+        board: core.getInput('jira-board', {required: true}),
+        token: core.getInput('repo-token', {required: true}),
+    }
+
     const request = {
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
@@ -9,11 +15,18 @@ try {
     }
 
     // const body = github.context.payload.pull_request.body || '';
-    const ticket = core.getInput("host") + "browse/" + core.getInput("board") + "-999";
+    const ticket = `${inputs.host}browse/${inputs.board}-999`;
     request.body = ({
         prefix: `# Jira ticket\n${ticket}`
     })
 
+    const octokit = github.getOctokit(inputs.token);
+    const response = await octokit.pulls.update(request);
+
+    core.info(`Response: ${response.status}`);
+    if (response.status !== 200) {
+        core.error('Updating the pull request has failed');
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
