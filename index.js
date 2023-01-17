8ea3b85322c68d30d4ce08733c96070186693ed2
Eddie Rusinskas <erusinskas@wonde.com>
2023-01-17 16:54:29 +0000

    Test

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 async function run() {
     try {
         const inputs = {
-            host: core.getInput('jira-host', {required: true}),
-            board: core.getInput('jira-board', {required: true}),
-            token: core.getInput('repo-token', {required: true}),
+            jira_host: core.getInput('jira-host', {required: true}),
+            jira_board: core.getInput('jira-board', {required: true}),
+            jira_token: core.getInput('jira-token', {required: true}),
+            github_token: core.getInput('repo-token', {required: true}),
         }
 
         const { Octokit } = require("@octokit/rest");
@@ -20,27 +22,36 @@ async function run() {
         const title = github.context.payload.pull_request.title || '';
 
 
-        if(title.toUpperCase().startsWith(inputs.board)) {
-            const formattedTitle = title.replace(inputs.board + "-", "");
+        let ticket = null;
+        if(title.toUpperCase().startsWith(inputs.jira_board)) {
+            const formattedTitle = title.replace(inputs.jira_board + "-", "");
             let ticketNumberIndex = 1;
             while (!isNaN(formattedTitle.substring(0, ticketNumberIndex + 1))) {
                 ticketNumberIndex++;
             }
             const ticketNumber = formattedTitle.substring(0, ticketNumberIndex);
             const body = github.context.payload.pull_request.body || '';
-            const ticket = `${inputs.host}browse/${inputs.board}-${ticketNumber}`;
-            request.body = `# Jira ticket\n${ticket}\n` + body;
+            let ticket = `${inputs.jira_board}-${ticketNumber}`;
+            const ticketUrl = `${inputs.jira_host}browse/${ticket}`;
+            request.body = `# Jira issue\n${ticketUrl}\n` + body;
         }
 
-        const octokit = new Octokit({
-            auth: inputs.token
-        });
-        const response = await octokit.rest.pulls.update(request);
+        if(ticket) {
+            const octokit = new Octokit({
+                auth: inputs.github_token
+            });
+            const response = await octokit.rest.pulls.update(request);
 
-        core.info(`Response: ${response.status}`);
-        if (response.status !== 200) {
-            core.error('Updating the pull request has failed');
+            axios.post(`${inputs.jira_host}/rest/api/3/issue/${ticket}/comment`, {
+                body: "Pull Request has been opened: " + github.context.payload.pull_request.html_url
+            });
+
+            core.info(`Response: ${response.status}`);
+            if (response.status !== 200) {
+                core.error('Updating the pull request has failed');
+            }
         }
+
     } catch (error) {
         core.error(error)
         core.setFailed(error.message);
