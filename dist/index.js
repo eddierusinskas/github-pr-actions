6450e7ce2924237cc8964217816bd72e807858a5
Eddie Rusinskas <erusinskas@wonde.com>
2023-01-17 15:30:13 +0000

    Test

--- dist/index.js
+++ dist/index.js
@@ -9775,21 +9775,36 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(312);
 const github = __nccwpck_require__(3370);
 
-try {
-    const request = {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: github.context.payload.pull_request.number,
+async function run() {
+    try {
+        const inputs = {
+            host: core.getInput('jira-host', {required: true}),
+            board: core.getInput('jira-board', {required: true}),
+            token: core.getInput('repo-token', {required: true}),
+        }
+
+        const request = {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: github.context.payload.pull_request.number,
+        }
+
+        // const body = github.context.payload.pull_request.body || '';
+        const ticket = `${inputs.host}browse/${inputs.board}-999`;
+        request.body = ({
+            prefix: `# Jira ticket\n${ticket}`
+        })
+
+        const octokit = github.getOctokit(inputs.token);
+        const response = await octokit.pulls.update(request);
+
+        core.info(`Response: ${response.status}`);
+        if (response.status !== 200) {
+            core.error('Updating the pull request has failed');
+        }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    // const body = github.context.payload.pull_request.body || '';
-    const ticket = core.getInput("host") + "browse/" + core.getInput("board") + "-999";
-    request.body = ({
-        prefix: `# Jira ticket\n${ticket}`
-    })
-
-} catch (error) {
-    core.setFailed(error.message);
 }
 
 })();
--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    const inputs = {
-        host: core.getInput('jira-host', {required: true}),
-        board: core.getInput('jira-board', {required: true}),
-        token: core.getInput('repo-token', {required: true}),
+async function run() {
+    try {
+        const inputs = {
+            host: core.getInput('jira-host', {required: true}),
+            board: core.getInput('jira-board', {required: true}),
+            token: core.getInput('repo-token', {required: true}),
+        }
+
+        const request = {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: github.context.payload.pull_request.number,
+        }
+
+        // const body = github.context.payload.pull_request.body || '';
+        const ticket = `${inputs.host}browse/${inputs.board}-999`;
+        request.body = ({
+            prefix: `# Jira ticket\n${ticket}`
+        })
+
+        const octokit = github.getOctokit(inputs.token);
+        const response = await octokit.pulls.update(request);
+
+        core.info(`Response: ${response.status}`);
+        if (response.status !== 200) {
+            core.error('Updating the pull request has failed');
+        }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    const request = {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        pull_number: github.context.payload.pull_request.number,
-    }
-
-    // const body = github.context.payload.pull_request.body || '';
-    const ticket = `${inputs.host}browse/${inputs.board}-999`;
-    request.body = ({
-        prefix: `# Jira ticket\n${ticket}`
-    })
-
-    const octokit = github.getOctokit(inputs.token);
-    const response = await octokit.pulls.update(request);
-
-    core.info(`Response: ${response.status}`);
-    if (response.status !== 200) {
-        core.error('Updating the pull request has failed');
-    }
-} catch (error) {
-    core.setFailed(error.message);
 }
